d05d2ab2b500e8b99876411d94390c3fe13a8f7a
enbirr <nbir94@gmail.com>
2024-01-19 20:43:26 +0100

    XPLN-13 Connect ModelController to ModelService

--- src/main/kotlin/app/xplne/api/controller/ModelController.kt
+++ src/main/kotlin/app/xplne/api/controller/ModelController.kt
@@ -2,6 +2,7 @@ package app.xplne.api.controller
 
 import app.xplne.api.dto.ModelFullDto
 import app.xplne.api.dto.ModelShortView
+import app.xplne.api.service.ModelService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
@@ -10,25 +11,25 @@ import java.util.*
 @RestController
 @RequestMapping("/v1/models")
 @Tag(name = "Models")
-class ModelController {
+class ModelController(
+    private val modelService: ModelService
+) {
 
     @GetMapping
     @Operation(summary = "Get all models")
     fun findAllModels(): List<ModelShortView> {
-        // TODO implement interaction with DB
-        return emptyList()
+        return modelService.findAll()
     }
 
     @GetMapping("/{modelId}")
     @Operation(summary = "Get model by ID")
     fun getModelById(@PathVariable modelId: UUID): ModelFullDto? {
-        // TODO implement interaction with DB
-        return null
+        return modelService.findByIdOrNull(modelId)
     }
 
     @DeleteMapping("/{modelId}")
     @Operation(summary = "Delete Model")
     fun deleteModel(@PathVariable modelId: UUID) {
-        // TODO implement interaction with DB
+        modelService.deleteById(modelId)
     }
 }
